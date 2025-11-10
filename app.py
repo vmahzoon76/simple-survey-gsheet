1cd6351e83a4ea5e707862beead8e3d3a3190e43
vmahzoon76 <125705447+vmahzoon76@users.noreply.github.com>
2025-11-10 10:42:55 -0500

    Update iframe source URL in app.py

--- app.py
+++ app.py
@@ -602,7 +602,7 @@ if not st.session_state.entered:
         """
         <div style="text-align:center;">
             <iframe 
-                src="https://drive.google.com/file/d/12AuUzfG1QBXULffbfPWcmgXR_40rp4Kh/preview" 
+                src="https://drive.google.com/file/d/1iqIQelHKuTaHVpVunttcSJ8zMiZ7rxyg/preview" 
                 width="800" height="450" frameborder="0" allowfullscreen>
             </iframe>
         </div>
