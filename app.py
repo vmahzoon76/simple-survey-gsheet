edee9f01b2c98dc821ad2175acf1709e0599b6bf
vmahzoon76 <125705447+vmahzoon76@users.noreply.github.com>
2026-02-17 07:27:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -988,7 +988,7 @@ with right:
                 color="#7c3aed",
                 size=200,
                 filled=True,
-                dy=-10  # shift up so the tip of triangle lands on the value
+                dy=-15  # shift up so the tip of triangle lands on the value
             ).encode(
                 x=alt.X("x:Q"),
                 y=alt.Y("y:Q"),
@@ -999,7 +999,7 @@ with right:
                 color="#7c3aed",
                 fontSize=11,
                 fontWeight="bold",
-                dy=-28,  # shift text further up above the triangle
+                dy=-18,  # shift text further up above the triangle
                 dx=5
             ).encode(
                 x=alt.X("x:Q"),
